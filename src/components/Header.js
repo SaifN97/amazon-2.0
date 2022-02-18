ed8ee1bdc0b106d2b6c30a0d627fd5d7af5423d9
SaifN97 <saifnarpali@gmail.com>
2022-02-18 18:51:10 +0530

    Checkout page and cart logic with redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,21 @@ import {
 } from "@heroicons/react/outline";
 
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
 const Header = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+  const items = useSelector(selectItems);
   return (
     <header>
       {/* Top nav */}
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             alt="logo"
             width={150}
@@ -45,9 +51,12 @@ const Header = () => {
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
 
-          <div className="relative link flex items-center">
+          <div
+            onClick={() => router.push("/checkout")}
+            className="relative link flex items-center"
+          >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
-              0
+              {items.length}
             </span>
             <ShoppingCartIcon className="h-10" />
             <p className="hidden  md:inline font-extrabold md:text-sm mt-2">
